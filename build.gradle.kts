b67e0bd0688a33ffa7ebf2f3f9c99f21baaee983
Anton Bashirov <antonbashir@mail.ru>
2019-11-17 00:55:12 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import org.gradle.api.JavaVersion.*
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0awdawdwa
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,7 +18,7 @@ import org.gradle.api.JavaVersion.*
  * limitations under the License.
  */
 
-plugins {
+plugins {awdawdaw
     id("io.github.art.project") version "1.0.97"
 }
 
