67c6ee7c67aac12951872b55adc695a47e21a9f3
antonbashir <antonbashir@mail.ru>
2019-11-20 12:04:35 +0300

    add example

--- src/main/kotlin/ru/art/example/Main.kt
+++ src/main/kotlin/ru/art/example/Main.kt
@@ -1,12 +1,44 @@
 package ru.art.example
 
 import ru.art.config.extensions.activator.AgileConfigurationsActivator.*
+import ru.art.http.constants.MimeToContentTypeMapper.*
 import ru.art.http.server.HttpServer.*
+import ru.art.http.server.function.HttpServiceFunction.*
+import ru.art.http.server.module.HttpServerModule.*
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         useAgileConfigurations("example")
+
+        httpGet(httpServerModule().path)
+                .producesMimeType(textHtmlUtf8())
+                .produce { "<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "  <head>\n" +
+                        "    <meta charset=\"UTF-8\" />\n" +
+                        "    <title>Add React in One Minute</title>\n" +
+                        "  </head>\n" +
+                        "  <body>\n" +
+                        "\n" +
+                        "    <h2>Add React in One Minute</h2>\n" +
+                        "    <p>This page demonstrates using React with no build tooling.</p>\n" +
+                        "    <p>React is loaded as a script tag.</p>\n" +
+                        "\n" +
+                        "    <!-- We will put our React component inside this div. -->\n" +
+                        "    <div id=\"like_button_container\"></div>\n" +
+                        "\n" +
+                        "    <!-- Load React. -->\n" +
+                        "    <!-- Note: when deploying, replace \"development.js\" with \"production.min.js\". -->\n" +
+                        "    <script src=\"https://unpkg.com/react@16/umd/react.development.js\" crossorigin></script>\n" +
+                        "    <script src=\"https://unpkg.com/react-dom@16/umd/react-dom.development.js\" crossorigin></script>\n" +
+                        "\n" +
+                        "    <!-- Load our React component. -->\n" +
+                        "    <script src=\"like_button.js\"></script>\n" +
+                        "\n" +
+                        "  </body>\n" +
+                        "</html>" }
+
         startHttpServer().await()
     }
 }
