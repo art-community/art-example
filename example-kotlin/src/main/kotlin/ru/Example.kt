85da9dfb2ab9478064ff0a3bb1d46ded21d4c262
anton.bashirov <anton.sh.local@gmail.com>
2021-08-16 00:50:29 +0300

    ART

--- example-kotlin/src/main/kotlin/ru/Example.kt
+++ example-kotlin/src/main/kotlin/ru/Example.kt
@@ -1,8 +1,11 @@
 package ru
 
+import io.art.communicator.configurator.CommunicatorActionConfigurator
 import io.art.launcher.kotlin.*
 import io.art.logging.kotlin.info
 import io.art.rsocket.kotlin.rsocketConnector
+import io.art.server.configurator.ServiceMethodConfigurator
+import ru.communicator.MyCommunicator
 import ru.communicator.MyConnector
 import ru.meta.MetaExampleKotlin
 import ru.model.Model
@@ -16,8 +19,16 @@ fun main() = activator {
     messagePack()
     transport()
     rsocket {
-        server { server -> server.tcp().configureService(MyService::class.java) }
-        communicator { communicator -> communicator.tcp(MyConnector::class.java) }
+        server { server ->
+            server
+                    .tcp()
+                    .configureService(MyService::class.java, ServiceMethodConfigurator::logging)
+        }
+        communicator { communicator ->
+            communicator
+                    .tcp(MyConnector::class.java)
+                    .configureCommunicator(MyCommunicator::class.java, CommunicatorActionConfigurator::logging)
+        }
     }
     onLaunch {
         rsocketConnector<MyConnector> {
