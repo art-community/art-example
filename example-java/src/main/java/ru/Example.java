99795a188d990c98be0694fc037686a194caa8d9
anton.bashirov <anton.sh.local@gmail.com>
2021-10-23 03:18:13 +0300

    ART

--- example-java/src/main/java/ru/Example.java
+++ example-java/src/main/java/ru/Example.java
@@ -1,27 +1,14 @@
 package ru;
 
-import io.art.http.*;
 import io.netty.channel.epoll.*;
-import ru.communicator.*;
-import ru.meta.*;
-import ru.model.*;
-import ru.service.*;
-import static io.art.configurator.module.ConfiguratorActivator.*;
-import static io.art.http.module.HttpActivator.*;
-import static io.art.json.module.JsonActivator.*;
-import static io.art.launcher.Activator.*;
-import static io.art.logging.Logging.*;
-import static io.art.logging.module.LoggingActivator.*;
-import static io.art.message.pack.module.MessagePackActivator.*;
-import static io.art.meta.module.MetaActivator.*;
-import static io.art.rsocket.Rsocket.*;
-import static io.art.rsocket.module.RsocketActivator.*;
-import static io.art.transport.module.TransportActivator.*;
-import static io.art.yaml.module.YamlActivator.*;
+import io.netty.channel.unix.FileDescriptor;
 import java.io.*;
 
 public class Example {
     public static void main(String[] arguments) throws IOException {
-        Native.newEpollCreate().close();
+        FileDescriptor.from(".").isOpen();
+        System.out.println("open");
+        FileDescriptor.from(".").close();
+        System.out.println("close");
     }
 }
