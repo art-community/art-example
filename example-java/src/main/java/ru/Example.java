ed7286724388cdefebb5e4623b4c0679b9b458ae
anton.bashirov <anton.sh.local@gmail.com>
2021-10-24 01:24:21 +0300

    ART

--- example-java/src/main/java/ru/Example.java
+++ example-java/src/main/java/ru/Example.java
@@ -1,14 +1,14 @@
 package ru;
 
-import io.netty.channel.epoll.*;
 import io.netty.channel.unix.FileDescriptor;
 import io.netty.channel.unix.*;
-import static io.netty.channel.epoll.Native.KERNEL_VERSION;
+import static io.netty.channel.epoll.Native.*;
 import java.io.*;
 
 public class Example {
     public static void main(String[] arguments) throws IOException {
         System.out.println(KERNEL_VERSION);
+        System.out.println(Errors.ERRNO_EAGAIN_NEGATIVE);
         System.out.println(Limits.IOV_MAX);
         FileDescriptor.from(".").isOpen();
         System.out.println("open");
