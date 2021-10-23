0c24aac6bbba77c83ebc6f09e4c7cba4dfaa53bc
anton.bashirov <anton.sh.local@gmail.com>
2021-10-24 01:03:55 +0300

    ART

--- example-java/src/main/java/ru/Example.java
+++ example-java/src/main/java/ru/Example.java
@@ -1,11 +1,13 @@
 package ru;
 
+import io.netty.channel.epoll.*;
 import io.netty.channel.unix.*;
 import io.netty.channel.unix.FileDescriptor;
 import java.io.*;
 
 public class Example {
     public static void main(String[] arguments) throws IOException {
+        System.out.println(Native.EPOLLIN);
         System.out.println(Limits.IOV_MAX);
         FileDescriptor.from(".").isOpen();
         System.out.println("open");
