a73175ef8fb0e6296c1f244777dc522edf381801
anton.bashirov <anton.sh.local@gmail.com>
2021-10-24 00:55:50 +0300

    ART

--- example-java/src/main/java/ru/Example.java
+++ example-java/src/main/java/ru/Example.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class Example {
     public static void main(String[] arguments) throws IOException {
         System.out.println(Limits.IOV_MAX);
-        FILEDESCRIPTOR.from(".").isOpen();
+        FileDescriptor.from(".").isOpen();
         System.out.println("open");
         FileDescriptor.from(".").close();
         System.out.println("close");
