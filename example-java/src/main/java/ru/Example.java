beb5fe2e1bac13115b0b42b44985e453d4f33256
anton.bashirov <anton.sh.local@gmail.com>
2021-10-24 01:55:27 +0300

    ART

--- example-java/src/main/java/ru/Example.java
+++ example-java/src/main/java/ru/Example.java
@@ -1,18 +1,50 @@
 package ru;
 
-import io.netty.channel.unix.FileDescriptor;
-import io.netty.channel.unix.*;
-import static io.netty.channel.epoll.Native.*;
-import java.io.*;
+import io.art.http.*;
+import ru.communicator.*;
+import ru.meta.*;
+import ru.model.*;
+import ru.service.*;
+import static io.art.configurator.module.ConfiguratorActivator.*;
+import static io.art.http.module.HttpActivator.*;
+import static io.art.json.module.JsonActivator.*;
+import static io.art.launcher.Activator.*;
+import static io.art.logging.Logging.*;
+import static io.art.logging.module.LoggingActivator.*;
+import static io.art.message.pack.module.MessagePackActivator.*;
+import static io.art.meta.module.MetaActivator.*;
+import static io.art.rsocket.Rsocket.*;
+import static io.art.rsocket.module.RsocketActivator.*;
+import static io.art.transport.module.TransportActivator.*;
+import static io.art.yaml.module.YamlActivator.*;
 
 public class Example {
-    public static void main(String[] arguments) throws IOException {
-        System.out.println(KERNEL_VERSION);
-        System.out.println(Errors.ERRNO_EAGAIN_NEGATIVE);
-        System.out.println(Limits.IOV_MAX);
-        FileDescriptor.from(".").isOpen();
-        System.out.println("open");
-        FileDescriptor.from(".").close();
-        System.out.println("close");
+    public static void main(String[] arguments) {
+        activator(arguments)
+                .main(Example.class.getSimpleName())
+                .module(meta(MetaExampleJava::new))
+                .module(configurator())
+                .module(logging())
+                .module(messagePack())
+                .module(json())
+                .module(yaml())
+                .module(transport())
+                .module(rsocket(rsocket -> rsocket
+                        .server(server -> server.tcp().service(MyService.class))
+                        .communicator(communicator -> communicator.tcp(MyConnector.class))))
+                .module(http(http -> http
+                        .server(server -> server.route(MyService.class))
+                        .communicator(communicator -> communicator.connector(MyConnector.class))))
+                .onLaunch(() -> {
+                    logger().info(rsocketConnector(MyConnector.class)
+                            .my()
+                            .myMethod(Model.builder().value("request").build())
+                            .toString());
+                    logger().info(Http.httpConnector(MyConnector.class)
+                            .my()
+                            .getModel()
+                            .toString());
+                })
+                .launch();
     }
 }
