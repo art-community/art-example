6faf87eb3c325425a8166a4a2f987d3bce45d035
Anton Bashirov <anton.bashirov@rt.ru>
2022-02-15 16:47:16 +0300

    ART

--- example-kotlin/src/main/kotlin/ru/Example.kt
+++ example-kotlin/src/main/kotlin/ru/Example.kt
@@ -12,6 +12,7 @@ import io.art.transport.kotlin.transport
 import io.art.yaml.kotlin.yaml
 import ru.communicator.MyPortal
 import ru.meta.MetaExampleKotlin
+import ru.meta.MetaExampleKotlin.MetaRuPackage.MetaServicePackage.MetaMyServiceClass.Companion.myService
 import ru.service.MyService
 
 data class Request(val success: String)
@@ -29,7 +30,13 @@ fun main() = activator {
         communicator { communicator -> communicator.tcp(MyPortal::class.java) }
     }
     http {
-        server { server -> server.routes(MyService::class.java) }
+        server { server ->
+            server
+                    .routes(MyService::class.java)
+                    .routes { myService() }
+                    .route { myService().compensationMethod() }
+                    .route { myService().getModelMethod() }
+        }
         communicator { communicator -> communicator.portal(MyPortal::class.java) }
     }
     launch().block()
