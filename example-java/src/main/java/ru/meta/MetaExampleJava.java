756e022669a138ab8b16c9f747c97f2a6c3e206b
anton <anton.sh.local@gmail.com>
2024-07-07 18:41:24 +0400

    fixes

--- example-java/src/main/java/ru/meta/MetaExampleJava.java
+++ example-java/src/main/java/ru/meta/MetaExampleJava.java
@@ -7,7 +7,6 @@ import static io.art.meta.model.MetaType.metaType;
 import io.art.core.property.LazyProperty;
 import io.art.meta.model.InstanceMetaMethod;
 import io.art.meta.model.MetaClass;
-import io.art.meta.model.MetaConstructor;
 import io.art.meta.model.MetaField;
 import io.art.meta.model.MetaLibrary;
 import io.art.meta.model.MetaMethod;
@@ -62,8 +61,6 @@ public class MetaExampleJava extends MetaLibrary {
     public static final class MetaExampleClass extends MetaClass<ru.Example> {
       private static final LazyProperty<MetaExampleClass> self = MetaClass.self(ru.Example.class);
 
-      private final MetaConstructorConstructor constructor = registerConstructor(new MetaConstructorConstructor(this));
-
       private final MetaMainMethod mainMethod = registerMethod(new MetaMainMethod(this));
 
       private MetaExampleClass() {
@@ -74,28 +71,8 @@ public class MetaExampleJava extends MetaLibrary {
         return self.get();
       }
 
-      public MetaConstructorConstructor constructor() {
-        return constructor;
-      }
-
       public MetaMainMethod mainMethod() {
         return mainMethod;
-      }
-
-      public final class MetaConstructorConstructor extends MetaConstructor<MetaExampleClass, ru.Example> {
-        private MetaConstructorConstructor(MetaExampleClass owner) {
-          super(metaType(ru.Example.class),owner);
-        }
-
-        @Override
-        public ru.Example invoke(java.lang.Object[] arguments) throws Throwable {
-          return new ru.Example();
-        }
-
-        @Override
-        public ru.Example invoke() throws Throwable {
-          return new ru.Example();
-        }
       }
 
       public final class MetaMainMethod extends StaticMetaMethod<MetaExampleClass, Void> {
@@ -137,8 +114,6 @@ public class MetaExampleJava extends MetaLibrary {
       public static final class MetaMyServiceClass extends MetaClass<ru.service.MyService> {
         private static final LazyProperty<MetaMyServiceClass> self = MetaClass.self(ru.service.MyService.class);
 
-        private final MetaConstructorConstructor constructor = registerConstructor(new MetaConstructorConstructor(this));
-
         private final MetaMyMethodMethod myMethodMethod = registerMethod(new MetaMyMethodMethod(this));
 
         private final MetaGetModelMethod getModelMethod = registerMethod(new MetaGetModelMethod(this));
@@ -151,32 +126,12 @@ public class MetaExampleJava extends MetaLibrary {
           return self.get();
         }
 
-        public MetaConstructorConstructor constructor() {
-          return constructor;
-        }
-
         public MetaMyMethodMethod myMethodMethod() {
           return myMethodMethod;
         }
 
         public MetaGetModelMethod getModelMethod() {
           return getModelMethod;
-        }
-
-        public final class MetaConstructorConstructor extends MetaConstructor<MetaMyServiceClass, ru.service.MyService> {
-          private MetaConstructorConstructor(MetaMyServiceClass owner) {
-            super(metaType(ru.service.MyService.class),owner);
-          }
-
-          @Override
-          public ru.service.MyService invoke(java.lang.Object[] arguments) throws Throwable {
-            return new ru.service.MyService();
-          }
-
-          @Override
-          public ru.service.MyService invoke() throws Throwable {
-            return new ru.service.MyService();
-          }
         }
 
         public final class MetaMyMethodMethod extends InstanceMetaMethod<MetaMyServiceClass, ru.service.MyService, ru.model.Model> {
@@ -342,8 +297,6 @@ public class MetaExampleJava extends MetaLibrary {
       public static final class MetaModelClass extends MetaClass<ru.model.Model> {
         private static final LazyProperty<MetaModelClass> self = MetaClass.self(ru.model.Model.class);
 
-        private final MetaConstructorConstructor constructor = registerConstructor(new MetaConstructorConstructor(this));
-
         private final MetaField<MetaModelClass, java.lang.String> valueField = registerField(new MetaField<>("value",metaType(java.lang.String.class),false,this));
 
         private final MetaToBuilderMethod toBuilderMethod = registerMethod(new MetaToBuilderMethod(this));
@@ -360,10 +313,6 @@ public class MetaExampleJava extends MetaLibrary {
           return self.get();
         }
 
-        public MetaConstructorConstructor constructor() {
-          return constructor;
-        }
-
         public MetaField<MetaModelClass, java.lang.String> valueField() {
           return valueField;
         }
@@ -378,28 +327,6 @@ public class MetaExampleJava extends MetaLibrary {
 
         public MetaModelBuilderClass modelBuilderClass() {
           return modelBuilderClass;
-        }
-
-        public final class MetaConstructorConstructor extends MetaConstructor<MetaModelClass, ru.model.Model> {
-          private final MetaParameter<java.lang.String> valueParameter = register(new MetaParameter<>(0, "value",metaType(java.lang.String.class)));
-
-          private MetaConstructorConstructor(MetaModelClass owner) {
-            super(metaType(ru.model.Model.class),owner);
-          }
-
-          @Override
-          public ru.model.Model invoke(java.lang.Object[] arguments) throws Throwable {
-            return new ru.model.Model((java.lang.String)(arguments[0]));
-          }
-
-          @Override
-          public ru.model.Model invoke(java.lang.Object argument) throws Throwable {
-            return new ru.model.Model((java.lang.String)(argument));
-          }
-
-          public MetaParameter<java.lang.String> valueParameter() {
-            return valueParameter;
-          }
         }
 
         public final class MetaToBuilderMethod extends InstanceMetaMethod<MetaModelClass, ru.model.Model, ru.model.Model.ModelBuilder> {
